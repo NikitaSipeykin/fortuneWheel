bd75fe9e834623b27d4d8c007df151826331b347
MacNikSipeykin <niksipeykin@gmail.com>
2024-12-16 11:35:51 +0300

    realise character wheel functional

--- js/app.js
+++ js/app.js
@@ -1,37 +1,269 @@
 // надписи и цвета на секторах
 const prizes = [
   {
-    text: "Скидка 10%",
-    color: "hsl(197 30% 43%)",
+    text: "1",
+    img: "images/characters/1.jpg",
   },
   { 
-    text: "Дизайн в подарок",
-    color: "hsl(173 58% 39%)",
+    text: "2",
+    img: "images/characters/2.jpg",
+  },
+  {
+    text: "3",
+    img: "images/characters/3.jpg",
   },
   { 
-    text: "Второй сайт бесплатно",
-    color: "hsl(43 74% 66%)",
+    text: "4",
+    img: "images/characters/4.jpg",
   },
   {
-    text: "Скидка 50%",
-    color: "hsl(27 87% 67%)",
+    text: "5",
+    img: "images/characters/5.jpg",
+  },
+  { 
+    text: "6",
+    img: "images/characters/6.jpg",
   },
   {
-    text: "Блог в подарок",
-    color: "hsl(12 76% 61%)",
+    text: "7",
+    img: "images/characters/7.jpg",
+  },
+  { 
+    text: "8",
+    img: "images/characters/8.jpg",
   },
   {
-    text: "Скидок нет",
-    color: "hsl(350 60% 52%)",
+    text: "9",
+    img: "images/characters/9.jpg",
+  },
+  { 
+    text: "10",
+    img: "images/characters/10.jpg",
   },
   {
-    text: "Таргет в подарок",
-    color: "hsl(91 43% 54%)",
+    text: "11",
+    img: "images/characters/11.jpg",
+  },
+  { 
+    text: "12",
+    img: "images/characters/12.jpg",
   },
   {
-    text: "Скидка 30% на всё",
-    color: "hsl(140 36% 74%)",
-  }
+    text: "13",
+    img: "images/characters/13.jpg",
+  },
+  { 
+    text: "14",
+    img: "images/characters/14.jpg",
+  },
+  {
+    text: "15",
+    img: "images/characters/15.jpg",
+  },
+  { 
+    text: "16",
+    img: "images/characters/16.jpg",
+  },
+  {
+    text: "17",
+    img: "images/characters/17.jpg",
+  },
+  { 
+    text: "18",
+    img: "images/characters/18.jpg",
+  },
+  {
+    text: "19",
+    img: "images/characters/19.jpg",
+  },
+  { 
+    text: "20",
+    img: "images/characters/20.jpg",
+  },
+  {
+    text: "21",
+    img: "images/characters/21.jpg",
+  },
+  { 
+    text: "22",
+    img: "images/characters/22.jpg",
+  },
+  {
+    text: "23",
+    img: "images/characters/23.jpg",
+  },
+  { 
+    text: "24",
+    img: "images/characters/24.jpg",
+  },
+  {
+    text: "25",
+    img: "images/characters/25.jpg",
+  },
+  { 
+    text: "26",
+    img: "images/characters/26.jpg",
+  },
+  {
+    text: "27",
+    img: "images/characters/27.jpg",
+  },
+  { 
+    text: "28",
+    img: "images/characters/28.jpg",
+  },
+  {
+    text: "29",
+    img: "images/characters/29.jpg",
+  },
+  { 
+    text: "30",
+    img: "images/characters/30.jpg",
+  },
+  {
+    text: "31",
+    img: "images/characters/31.jpg",
+  },
+  { 
+    text: "32",
+    img: "images/characters/32.jpg",
+  },
+  {
+    text: "33",
+    img: "images/characters/33.jpg",
+  },
+  { 
+    text: "34",
+    img: "images/characters/34.jpg",
+  },
+  {
+    text: "35",
+    img: "images/characters/35.jpg",
+  },
+  { 
+    text: "36",
+    img: "images/characters/36.jpg",
+  },
+  {
+    text: "37",
+    img: "images/characters/37.jpg",
+  },
+  { 
+    text: "38",
+    img: "images/characters/38.jpg",
+  },
+  {
+    text: "39",
+    img: "images/characters/39.jpg",
+  },
+  { 
+    text: "40",
+    img: "images/characters/40.jpg",
+  },
+  {
+    text: "41",
+    img: "images/characters/41.jpg",
+  },
+  { 
+    text: "42",
+    img: "images/characters/42.jpg",
+  },
+  {
+    text: "43",
+    img: "images/characters/43.jpg",
+  },
+  { 
+    text: "44",
+    img: "images/characters/44.jpg",
+  },
+  {
+    text: "45",
+    img: "images/characters/45.jpg",
+  },
+  { 
+    text: "46",
+    img: "images/characters/46.jpg",
+  },
+  {
+    text: "47",
+    img: "images/characters/47.jpg",
+  },
+  { 
+    text: "48",
+    img: "images/characters/48.jpg",
+  },
+  {
+    text: "49",
+    img: "images/characters/49.jpg",
+  },
+  { 
+    text: "50",
+    img: "images/characters/50.jpg",
+  },
+  {
+    text: "51",
+    img: "images/characters/51.jpg",
+  },
+  { 
+    text: "52",
+    img: "images/characters/52.jpg",
+  },
+  {
+    text: "53",
+    img: "images/characters/53.jpg",
+  },
+  { 
+    text: "54",
+    img: "images/characters/54.jpg",
+  },
+  {
+    text: "55",
+    img: "images/characters/55.jpg",
+  },
+  { 
+    text: "56",
+    img: "images/characters/56.jpg",
+  },
+  {
+    text: "57",
+    img: "images/characters/57.jpg",
+  },
+  { 
+    text: "58",
+    img: "images/characters/58.jpg",
+  },
+  {
+    text: "59",
+    img: "images/characters/59.jpg",
+  },
+  { 
+    text: "60",
+    img: "images/characters/60.jpg",
+  },
+  {
+    text: "61",
+    img: "images/characters/61.jpg",
+  },
+  { 
+    text: "62",
+    img: "images/characters/62.jpg",
+  },
+  {
+    text: "63",
+    img: "images/characters/63.jpg",
+  },
+  { 
+    text: "64",
+    img: "images/characters/64.jpg",
+  },
+  {
+    text: "65",
+    img: "images/characters/65.jpg",
+  },
+  { 
+    text: "66",
+    img: "images/characters/66.jpg",
+  },
 ];
 
 // создаём переменные для быстрого доступа ко всем объектам на странице — блоку в целом, колесу, кнопке и язычку
@@ -39,6 +271,8 @@ const wheel = document.querySelector(".deal-wheel");
 const spinner = wheel.querySelector(".spinner");
 const trigger = wheel.querySelector(".btn-spin");
 const ticker = wheel.querySelector(".ticker");
+const image = wheel.querySelector(".image");
+
 
 // на сколько секторов нарезаем круг
 const prizeSlice = 360 / prizes.length;
@@ -49,6 +283,8 @@ const spinClass = "is-spinning";
 const selectedClass = "selected";
 // получаем все значения параметров стилей у секторов
 const spinnerStyles = window.getComputedStyle(spinner);
+// Создаем переменную для блока, где будет отображаться изображение
+const resultImage = document.querySelector(".result-image");
 
 // переменная для анимации
 let tickerAnim;
@@ -138,12 +374,21 @@ const runTickerAnimation = () => {
 
 // функция выбора призового сектора
 const selectPrize = () => {
+  // Определяем выигрышный сектор
   const selected = Math.floor(rotation / prizeSlice);
+  // Подсвечиваем текущий сектор
   prizeNodes[selected].classList.add(selectedClass);
+  // Устанавливаем изображение, соответствующее выигрышному сектору
+  const prize = prizes[selected];
+  resultImage.innerHTML = `<img src="${prize.img}" alt="Prize">`;
+
+  resultImage.classList.remove("hidden");
 };
 
 // отслеживаем нажатие на кнопку
 trigger.addEventListener("click", () => {
+  // скрываем изображение
+  resultImage.classList.add("hidden");
   // делаем её недоступной для нажатия
   trigger.disabled = true;
   // задаём начальное вращение колеса
@@ -154,6 +399,7 @@ trigger.addEventListener("click", () => {
   wheel.classList.add(spinClass);
   // через CSS говорим секторам, как им повернуться
   spinner.style.setProperty("--rotate", rotation);
+  image.style.setProperty("--rotate", rotation);
   // возвращаем язычок в горизонтальную позицию
   ticker.style.animation = "none";
   // запускаем анимацию вращение
@@ -172,9 +418,11 @@ spinner.addEventListener("transitionend", () => {
   wheel.classList.remove(spinClass);
   // отправляем в CSS новое положение поворота колеса
   spinner.style.setProperty("--rotate", rotation);
+  image.style.setProperty("--rotate", rotation);
   // делаем кнопку снова активной
   trigger.disabled = false;
 });
+
 
 // подготавливаем всё к первому запуску
 setupWheel();
